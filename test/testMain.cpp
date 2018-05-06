471bbbad44d390152a4c973553578732aac4d7b3
Tobias Redl <huabatobs@gmail.com>
2018-05-06 20:56:07 +0200

    removed console output, while executing tests

--- test/testMain.cpp
+++ test/testMain.cpp
@@ -1,6 +1,8 @@
 #include "gtest/gtest.h"
 
 int main(int argc, char **argv) {
+    //disable cout by setting failbit, remove this line or call std::cout.clear()
+    std::cout.setstate(std::ios_base::failbit);
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
