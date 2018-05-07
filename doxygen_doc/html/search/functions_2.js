6164f73e5ba780337defb53adfe19fa4f9c6bd79
Travis CI <travis@travis-ci.org>
2018-05-07 09:33:43 +0000

    Deploy code docs to GitHub Pages Travis build: 53
    Commit: ec1aea86a3a482ba86a2f56d750e066733fefac5

--- doxygen_doc/html/search/functions_2.js
+++ doxygen_doc/html/search/functions_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['setcell',['setCell',['../class_sudoku_gitter.html#aa7fa54638ade311994bd8f6433df8ff0',1,'SudokuGitter']]],
   ['sudokugitter',['SudokuGitter',['../class_sudoku_gitter.html#ad3443d5846cba4cef8fdfebc46f16c0b',1,'SudokuGitter']]]
 ];
